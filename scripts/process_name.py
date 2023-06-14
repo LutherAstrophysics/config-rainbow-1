ddb1fb93a68ebdf92ae81ea2584747c0c2ad24cf
Gia Minh Nguyen <nguymi03@luther.edu>
2023-06-14 18:16:30 -0500

    Added scripts for writing process, renorm, and csv toml files

---
+++ scripts/process_name.py
@@ -0,0 +1,20 @@
+import sys
+from pathlib import Path
+from folders import get_nights_sorted
+
+
+year = sys.argv[1]
+year_folder = Path(f'H:/Raw Data/Summer {year}')
+
+nights = get_nights_sorted(year_folder)
+
+for night in nights: 
+    night_path = str(night.absolute())
+    night_path = night_path.replace("\\", "/")
+    print(f"""
+    [[input.nights]]
+    path = "{night_path}"
+    masterflat = "E:/Data Processing/Master Flats/{year}/{year}--_masterflat.fit"
+    """)
+
+    
