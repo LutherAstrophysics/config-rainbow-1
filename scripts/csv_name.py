afd9d31a042fe917e7a6449b3a1ceec7ea6e8aee
Gia Minh Nguyen <nguymi03@luther.edu>
2023-07-13 21:23:33 -0500

    Updated config files for couple of years

--- scripts/csv_name.py
+++ scripts/csv_name.py
@@ -1,8 +1,9 @@
+from folders import get_nights_sorted
 from pathlib import Path
 
-year_folder = Path('F:/Python_Processed/2021')
+year_folder = Path('E:/Data Processing/Python Processed/2005')
 
-for night in year_folder.glob('*'): 
+for night in get_nights_sorted(year_folder): 
     night_path = str(night.absolute())
     night_path = night_path.replace("\\", "/")
     print(f'"{night_path}",')
