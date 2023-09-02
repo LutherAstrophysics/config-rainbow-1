7b97e4e7a60eb7e46eb6e333f339aa694d56bb7c
Suman Chapai <sumanchapai@gmail.com>
2023-09-01 21:10:55 -0500

    Updates

--- scripts/csv_name.py
+++ scripts/csv_name.py
@@ -1,7 +1,7 @@
 from folders import get_nights_sorted
 from pathlib import Path
 
-year_folder = Path('E:/Data Processing/Python Processed/2005')
+year_folder = Path('E:/Data Processing/Python Processed/2003')
 
 for night in get_nights_sorted(year_folder): 
     night_path = str(night.absolute())
--- scripts/rename.py
+++ scripts/rename.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 # Kept inside run function to prevent from accidental execution
 def run():
-    folder_path = Path("E:\Data Processing\August 28, 2019\Flux Logs Combined\Four Pixel Radius")
-    for file in folder_path.glob('*'):
+    folder_path = Path(r"G:\Raw Data\Summer 2005\FOO\July 22, 2005\Calibration Frames")
+    for file in folder_path.glob('flat*'):
         old_name = file.name
-        new_name = old_name.replace('7.0_', '7.0-')
+        new_name = old_name.replace('2.50', '2.5-0')
         new_path = folder_path / new_name
         file.rename(new_path) 
 
