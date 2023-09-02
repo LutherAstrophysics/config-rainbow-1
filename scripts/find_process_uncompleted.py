7320962fea170997a42157bdf7471ea096b31fad
Suman Chapai <sumanchapai@gmail.com>
2023-09-02 10:12:44 -0500

    Add script to find process uncompleted nights

---
+++ scripts/find_process_uncompleted.py
@@ -0,0 +1,21 @@
+import sys
+from pathlib import Path
+from folders import get_nights_sorted
+
+
+year = sys.argv[1]
+year_folder = Path(f'E:/Data Processing/Python Processed/{year}/')
+
+nights = get_nights_sorted(year_folder)
+
+for night in nights: 
+    logger = list(night.glob("*Processing-log*"))
+    if len(logger) == 0 :
+        continue
+    logger = logger[0]
+    with open(logger, 'r') as fd:
+        lines = fd.readlines()
+        last_line = "".join(lines[-2:])
+        if "Completed drawing normfactors chart" not in last_line:
+            print(night)
+    
