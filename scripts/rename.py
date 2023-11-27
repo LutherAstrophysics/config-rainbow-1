dd3d4c70c32b3895b49696405bccbf731bba0c8c
Suman Chapai <sumanchapai@gmail.com>
2023-11-27 12:20:10 -0600

    Config files

--- scripts/rename.py
+++ scripts/rename.py
@@ -2,12 +2,21 @@ from pathlib import Path
 
 
 # Kept inside run function to prevent from accidental execution
-def run():
-    folder_path = Path(r"G:\Raw Data\Summer 2005\FOO\July 22, 2005\Calibration Frames")
-    for file in folder_path.glob('flat*'):
+def run(dry=True):
+    folder_path = Path(r"H:\Raw Data\Summer 2012\July 11, 2012\Calibration Frames")
+    for file in folder_path.glob('m23*'):
         old_name = file.name
-        new_name = old_name.replace('2.50', '2.5-0')
+        new_name = old_name.replace('m23', 'flat')
         new_path = folder_path / new_name
-        file.rename(new_path) 
+        if dry:
+            print("Old:", old_name)
+            print("New:", new_name, end="\n"*2)
+        else:
+            file.rename(new_path) 
 
-run()
+# Set this to true if you want to rename
+# Setting this to false will merely show you what the
+# renaming looks like.
+DESTRUCTIVE_RUN=False
+
+run(dry=not DESTRUCTIVE_RUN)
